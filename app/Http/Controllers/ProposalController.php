6a1b3a213d590a46209d0749c273a103bc4ec5a8
PiyumiD <piyumid@opusxenta.com>
2024-10-19 12:55:12 +0530

    Task 04 - feature/proposal - Create routes for proposal

---
+++ app/Http/Controllers/ProposalController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ProposalController extends Controller
+{
+    public function getAllProposals(){
+        dd('test');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 // use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProposalController;
+
+
+
+Route::prefix('proposal')->group(function () {
+    Route::get('get-all', [ProposalController::class, 'getAllProposals']);
+    Route::get('get-by-id/{id}', [ProposalController::class, 'getProposalById']);
+    Route::post('create', [ProposalController::class, 'createProposal']);
+    Route::post('edit/{id}', [ProposalController::class, 'editProposal']);
+});
+
 
 Route::get('{any?}', function () {
     return view('application');
